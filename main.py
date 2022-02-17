d72616994db5943931d34ae8fba7039a1cbe0bfa
Igor <84167672+lethalwombat@users.noreply.github.com>
2022-02-17 20:56:24 +1100

    changing logic to ad-hoc email cleaning

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import configparser
 from imaplib import IMAP4_SSL
 import email
 import os
-
+import re
 
 # get parameters from the config file
 configfile = configparser.ConfigParser()
-configfile.read(os.path.join(os.getcwd(), 'config.cfg'))
+configfile.read(os.path.join(os.getcwd(), 'dload_reminder', 'config.cfg'))
 
 # assing parameters to strings from configfile
 imap_host = configfile['connection']['host']
@@ -16,14 +16,28 @@ imap_user = configfile['connection']['user']
 imap_pass = configfile['connection']['password']
 
 folder = configfile['email']['folder']
-subj = configfile['email']['subject']
-purge = configfile['email']['purge']
+from_addr = configfile['email']['from'].lower()
+email_buffer = int(configfile['email']['buffer'])
 
-outfile = configfile['output']['outfile']
+# only purge if a specific email is set
+if from_addr == 'all':
+    purge = 'no'
+else:
+    purge = configfile['email']['purge']
 
-def read_dloads(imap_host, imap_user, imap_pass, folder, subj, purge: str) -> list:
+# function to parse email using regex
+def parse_email(input_str: str):
+    email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    _ = re.findall(email_regex, input_str)
+    if len(_) == 0:
+        return ''
+    else:
+        return _[0]
 
-    _results = []
+
+def read_dloads(imap_host, imap_user, imap_pass, email_buffer, folder, from_addr, purge: str) -> list:
+
+    _results = {}
 
     conn = IMAP4_SSL(imap_host)
     try:
@@ -32,28 +46,31 @@ def read_dloads(imap_host, imap_user, imap_pass, folder, subj, purge: str) -> li
         print(e)
     if conn.state == 'AUTH':
         status, messages = conn.select(folder)
-        _, selected_messages = conn.search(None, 'ALL')
+        n_messages = int(messages[0])
+        print('Total number of emails: {}'.format(n_messages))
+        print('Searching last {}'.format(email_buffer))
 
-        for m in selected_messages[0].split():
-            _, data = conn.fetch(m, '(RFC822)')
+        for i in range(n_messages, n_messages-email_buffer, -1):
+            _, data = conn.fetch(str(i), '(RFC822)')
             _, bytes_data = data[0]
-
             email_message = email.message_from_bytes(bytes_data)
-            _one_result = {'id' : int(m)}
 
-            if email_message['subject'].lower().startswith(subj.lower()):
+            # if searching all emails
+            parsed_email = parse_email(email_message['from']).lower()
+            if from_addr == 'all':
+                if parsed_email not in _results:
+                    _results[parsed_email] = 0
+                _results[parsed_email] += 1
+            
+            # if searching for a specific address
+            else:
+                if parsed_email == from_addr:
+                    if parsed_email not in _results:
+                        _results[parsed_email] = 0
+                    _results[parsed_email] += 1
 
-                _one_result['subject'] = email_message['subject']
-
-                for part in email_message.walk():
-                    if part.get_content_type() == 'text/plain':
-                        _one_result['body'] = part.get_payload(decode=True).decode()
-
-                _results.append(_one_result)
-
-
-    if purge == 'yes':
-        conn.store(m, "+FLAGS", "\\Deleted")
+    # if purge == 'yes':
+    #     conn.store(m, "+FLAGS", "\\Deleted")
             
     if conn.state == 'SELECTED':
         conn.expunge()
@@ -61,42 +78,18 @@ def read_dloads(imap_host, imap_user, imap_pass, folder, subj, purge: str) -> li
     conn.logout()
 
     return _results
-
-def dloads_to_file(dload_list: list, outfile: str) -> None:
-
-    if len(dload_list) == 0:
-        print('Nothing to add')
-        return None
-
-    else:
-        # check what is already on the list 
-        try:
-            with open(outfile, 'r', encoding='utf-8') as r:
-                on_the_list = [n.strip('\n') for n in r.readlines()]
-        except FileNotFoundError:
-            with open(outfile, 'w', encoding='utf-8') as r:
-                on_the_list = []
-            
-        # add new entries to the list
-        with open(outfile, 'a', encoding='utf-8') as f:        
-            for e in dload_list:
-                e_items = e['body'].strip('\n').split('\n')
-                for e_item in e_items:
-                    _ = e_item.strip('\r').lower()
-                    if _ not in on_the_list:
-                        on_the_list.append(_)
-                        f.write('{}\n'.format(_))
-        
-        return None
         
 
 def main():
-    # read in download reminders from email
-    dload_list = read_dloads(imap_host, imap_user, imap_pass, folder, subj, purge)
-    
-    # write results to the outputfile
-    dloads_to_file(dload_list, outfile)
+    print('Searching for emails from {} in {}:{}'.format(from_addr, folder, imap_user))
 
+    # read in download reminders from email
+    dload_list = read_dloads(imap_host, imap_user, imap_pass, email_buffer, folder, from_addr, purge)
+
+    # output results into a .csv file
+    with open('result_csv', 'w') as f:
+        for k, v in dload_list.items():
+            f.write('{},{}\n'.format(k, v))  
 
 if __name__ == '__main__':
-    main()
+    main()  
